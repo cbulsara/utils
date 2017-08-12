c2b30814ead0bd27d2e29f1c29269db7e91d7be2
cbulsara <bulsaracyrus@gmail.com>
2017-08-12 11:16:50 -0700

    asd

---
+++ parse_sep.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python2.7
+
+#---------------------------------------
+#parse_sep.py
+#
+#Scripting necessary to sanitize and
+#format CSV output from SEPM such that 
+#it is ingestible by Elasticsearch. 
+#Contains SEP-specific stuff, indicated
+#in comments. If using as a template for
+#other parsing functions be sure to take
+#SEP-specific stuff out or modify it.
+#---------------------------------------
+
+
+import pandas as pd
+import sys
+
+#Define the column names that we want to keep and in what order
+keep_cols = ['Computer_Name', 'Measure_Date', 
+            'Pattern_Date', 'Operating_System', 'Client_Version',
+            'Policy_Serial', 'HI_Status', 'Status',
+            'Auto_Protect_On', 'Worst_Detection', 
+            'Last_Scan_Time', 'Antivirus_engine_On',
+            'Download_Insight_On', 'SONAR_On', 'Tamper_Protection_On',
+            'Intrusion_Prevention_On', 'IE_Browser_Protection_On',
+            'Firefox_Browser_Protection_On', 'Early_Launch_Antimalware_On',
+             'Server_Name', 'MAC_Address1']
+
+def addMeasureDate(df):
+    measure_date = raw_input("Enter measurement date as MM/DD/YYYY: ")
+    df.insert(0, 'Measure_Date', measure_date)
+
+def pruneColumns(df):
+    df = df[keep_cols]
+
+def pruneDate(df):
+    col = raw_input("Enter name of date column to be pruned down to MM/DD/YYYY: ")
+    df[col].replace('Never', '', inplace=True)                                          ##SEP-specific: Elasticsearch can't handle text in date fields, so replace 'Never' (never scanned) with nothing
+    df[col] = df[col].map(lambda x: str(x).split(" ")[0])                               ##SEP-specific: Some date fields in SEPM will be formated MM/DD/YYYY + other junk separated by a space
+                                                                                                        #We want to get rid of everything after MM/DD/YYYY using space as delimeter
+def joinCMDB(file_path):
+    user_input = raw_input("Join with CMDB file [Y/N]: ").upper()
+
+    if user_input == "Y":
+        
+
+
+#-----------------------------------------, 
+#Main
+#
+#
+#-----------------------------------------
+
+#Check for args
+if len(sys.argv) == 1:
+    print "No args passed"
+    quit()
+
+for i in sys.argv[1:]:
+    
+    print("****" + i + "****")
+
+    df = pd.read_csv(i)
+    
+    addMeasureDate(df)
+
+    pruneColumns(df)
+
+    pruneDate(df)
+
